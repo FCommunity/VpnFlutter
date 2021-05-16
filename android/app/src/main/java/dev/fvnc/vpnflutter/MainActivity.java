e58178a1a99ac2ed4dd869145bb38a2dc095fbbf
Nguyen Van Hau <50167033+devhau@users.noreply.github.com>
2021-05-16 23:39:50 +0700

    Init Project
    Init Project

---
+++ android/app/src/main/java/dev/fvnc/vpnflutter/MainActivity.java
@@ -0,0 +1,6 @@
+package dev.fvnc.vpnflutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:vpnflutter/screens/home_page.dart';
+import 'package:vpnflutter/utils/custom_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Etornam VPN',
+      debugShowCheckedModeBanner: false,
+      theme: customLightTheme(context),
+      darkTheme: customDarkTheme(context),
+      themeMode: ThemeMode.system,
+      home: HomePage(),
+    );
+  }
+}
